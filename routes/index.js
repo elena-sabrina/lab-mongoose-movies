fe8fc071320a9b1b900dc67cadfef631b7f66f63
Elena Hadjimina <hadjimina.elena@gmail.com>
2021-01-22 21:31:10 +0800

    fixed file confusion. Iteration 1 done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const express = require("express");
+const Celebrity = require("./../models/celebrity");
+
 const router = express.Router();
 
 // Handle GET request for website root
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
 module.exports = router;
